eb36f39f250386bf8b33a8f1ad07206ef5692563
FoLenght <92605133+FoLenght@users.noreply.github.com>
2021-11-16 15:33:47 +0300

    Update Bash.nim

--- Bash.nim
+++ Bash.nim
@@ -1,9 +1,7 @@
 import std/os
 
-let username = "null";
-
 while true:
-    write(stdout, "\e[33m", "User=",username,";", "\e[0m", "\e[31m", "MadeBy:FoLen",
+    write(stdout, "\e[33m","User=FoLen;", "\e[0m", "\e[31m", "MadeBy:FoLen;",
             "\e[0m", "\e[90m", "Commands== ?% ", "\e[0m");
     var text = readLine(stdin);
     discard execShellCmd(text);
